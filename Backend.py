e15db2c93c2d076a3d78552ba9022716d6f9f6d7
thierry <thierry.senta@hotmail.fr>
2021-12-16 18:30:25 +0100

    update 16/12

--- Backend.py
+++ Backend.py
@@ -4,10 +4,9 @@
 #######################################################################################################################
 #######################################################################################################################
 
-
-#########################################################################################################
-###    00  -     PARAMETERS :                                                                         ###
-#########################################################################################################
+########################################################################################################################
+###    00  -     PARAMETERS :                                                                                        ###
+########################################################################################################################
 
 ###    00.1  -  Libraries :
 import os
@@ -69,15 +68,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # to disable the modificat
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)  # Cache time is set to 1 second, thus css and html reload when you change anything
 
 
-#########################################################################################################
-###    01  -     DATAMANAGEMENT :                                                                     ###
-#########################################################################################################
+########################################################################################################################
+###    01  -     DATAMANAGEMENT :                                                                                    ###
+########################################################################################################################
 df_activities = pd.read_csv(os.path.join(rep_data,activities_file))
 
 
-#########################################################################################################
-###    02  -     DB ENGINE and ROOTS :                                                                     ###
-#########################################################################################################
+########################################################################################################################
+###    02  -     DB ENGINE and ROOTS :                                                                               ###
+########################################################################################################################
 ###    02.1 -  Create the database Sqlite (once execute sqlite3.db in the repertory forever)
 db = SQLAlchemy()
 def init_db(app):
@@ -89,6 +88,17 @@ def create_DB():
     return 'Database created successful'
 
 ###   2.2  -     Create engine to connect with Sqlite
-engine2 = create_engine("sqlite:///sqlite3.db", encoding='utf-8')  # To final say the SQLAlchemy engine is created with Sqlite3
+engine = create_engine("sqlite:///sqlite3.db", encoding='utf-8')  # To final say the SQLAlchemy engine is created with Sqlite3
 #data_stock.to_sql('data_stock', con=engine2, if_exists='replace', index=False)
-data_target.to_sql('data_target', con=engine2, if_exists='replace', index=False)
+df_activities.to_sql('df_activities', con=engine, if_exists='replace', index=False)
+
+########################################################################################################################
+###    04  -     Run total Project DATAVIZ                                                                           ###
+########################################################################################################################
+import random, threading, webbrowser
+if __name__ == "__main__":
+    #app.run(host="0.0.0.0", port=8090, debug=True)  # Auto refresh page
+    port = 8090
+    url = "http://127.0.0.1:{0}".format(port)
+    threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+    app.run(port=8090, debug=True, use_reloader=False)  # Auto refresh page
